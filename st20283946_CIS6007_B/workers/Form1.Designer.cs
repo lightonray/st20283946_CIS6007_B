d45b67f4be9270dd15320136f6e17e7b11a384e2
rei kasemi <reikasemi2001@gmail.com>
2023-12-12 01:13:10 +0200

    first commit

---
+++ st20283946_CIS6007_B/workers/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace workers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxProgress = new System.Windows.Forms.ListBox();
+            this.btnStartPainting = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxProgress
+            // 
+            this.listBoxProgress.FormattingEnabled = true;
+            this.listBoxProgress.ItemHeight = 20;
+            this.listBoxProgress.Location = new System.Drawing.Point(1176, 69);
+            this.listBoxProgress.Name = "listBoxProgress";
+            this.listBoxProgress.Size = new System.Drawing.Size(615, 344);
+            this.listBoxProgress.TabIndex = 0;
+            // 
+            // btnStartPainting
+            // 
+            this.btnStartPainting.AccessibleDescription = "";
+            this.btnStartPainting.Font = new System.Drawing.Font("Sitka Heading", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnStartPainting.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnStartPainting.Location = new System.Drawing.Point(1392, 419);
+            this.btnStartPainting.Name = "btnStartPainting";
+            this.btnStartPainting.Size = new System.Drawing.Size(202, 61);
+            this.btnStartPainting.TabIndex = 1;
+            this.btnStartPainting.Text = "Start painting proccess";
+            this.btnStartPainting.UseVisualStyleBackColor = true;
+            this.btnStartPainting.Click += new System.EventHandler(this.btnStartPainting_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(793, 156);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(356, 24);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(793, 247);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(356, 24);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(793, 337);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(356, 24);
+            this.listBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(855, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 28);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number of team workers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(823, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(309, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number of circles to be painted";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(905, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Estimated time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(1307, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(352, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "TRACKED WORKERS PROGRESS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(731, 726);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1924, 750);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnStartPainting);
+            this.Controls.Add(this.listBoxProgress);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxProgress;
+        private System.Windows.Forms.Button btnStartPainting;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ st20283946_CIS6007_B/workers/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace workers
+{
+    public partial class Form1 : Form
+    {
+        private object lockObject = new object();
+        
+
+        private const int circleRadius = 4;
+        private const int circleSpacing = 10;
+
+        private int numberOfCircles = 1001;
+        private int numberOfWorkers = 100; // 4 // 20 [BASED ON THE NUMBER OF THE WORKERS]
+        private int circlePaintingTime = 20;
+
+        private HashSet<int> paintedIndices = new HashSet<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox2.Items.Add($"{numberOfCircles}");
+            listBox1.Items.Add($"{numberOfWorkers}");
+        }
+
+
+        private void PaintCircle(int workerId, int circleId)
+        {
+            Color color = GetThreadColor(workerId);
+
+            lock (lockObject)
+            {
+                paintedIndices.Add(circleId);
+            }
+
+            UpdateProgress($"Worker {workerId} is painting Circle {circleId}");
+
+            // Get the number of circles that can fit in rows and columns
+            int columns = pictureBox1.Width / (2 * circleRadius + circleSpacing);
+            int rows = pictureBox1.Height / (2 * circleRadius + circleSpacing);
+
+            int row = (circleId - 1) / columns;
+            int col = (circleId - 1) % columns;
+
+            int x = circleRadius + col * (circleRadius * 2 + circleSpacing);
+            int y = circleRadius * 3 + row * (circleRadius * 2 + circleSpacing);
+
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                DrawCircle(g, x, y, color);
+            }
+
+
+            Thread.Sleep(circlePaintingTime); // Simulate circle painting time
+        }
+
+        private void DrawCircle(Graphics g, int x, int y, Color color)
+        {
+            using (Brush brush = new SolidBrush(color))
+            {
+                g.FillEllipse(brush, x - circleRadius, y - circleRadius, 2 * circleRadius, 2 * circleRadius);
+            }
+        }
+
+        private Color GetThreadColor(int workerId)
+        {
+            Color[] colors = { Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Pink }; // Add more colors if needed
+            return colors[workerId % colors.Length];
+        }
+
+         private void UpdateProgress(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() =>
+                {
+                    listBoxProgress.Items.Add(message);
+                    listBoxProgress.SelectedIndex = listBoxProgress.Items.Count - 1;
+                }));
+            }
+            else
+            {
+                listBoxProgress.Items.Add(message);
+                listBoxProgress.SelectedIndex = listBoxProgress.Items.Count - 1;
+            }
+        }
+
+        private void UpdateListBox3(string message)
+        {
+            if (listBox3.InvokeRequired)
+            {
+                listBox3.Invoke(new Action(() => listBox3.Items.Add(message)));
+            }
+            else
+            {
+                listBox3.Items.Add(message);
+            }
+        }
+
+        private void ParallelPaintCircles(int numberOfCircles, int numberOfWorkers)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < numberOfWorkers; i++)
+            {
+                int workerStartIndex = i * (numberOfCircles / numberOfWorkers);
+                int workerEndIndex = (i == numberOfWorkers - 1) ? numberOfCircles : (i + 1) * (numberOfCircles / numberOfWorkers);
+
+                Thread thread = new Thread(() =>
+                {
+                    for (int circleId = workerStartIndex; circleId < workerEndIndex; circleId++)
+                    {
+                        if (!paintedIndices.Contains(circleId))
+                        {
+                            PaintCircle(Thread.CurrentThread.ManagedThreadId, circleId);
+                        }
+                    }
+                });
+
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join(); // Wait for threads to finish
+            }
+
+            stopwatch.Stop();
+
+            UpdateProgress("Circles were painted");
+            UpdateProgress("Completed Circles Count: " + paintedIndices.Count);
+
+            UpdateListBox3($"Elapsed Time: {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        private void btnStartPainting_Click_1(object sender, EventArgs e)
+        {
+            listBoxProgress.Items.Clear();
+            UpdateProgress("Start Painting Circles...");
+
+            Task.Run(() => ParallelPaintCircles(numberOfCircles, numberOfWorkers));
+        }
+
+
+    }
+}
